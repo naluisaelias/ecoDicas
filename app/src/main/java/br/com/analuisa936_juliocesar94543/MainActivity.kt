c50eb7751ee950dce56669640cc1d964f11066a7
Ana Luísa <naluisaelias@gmail.com>
2024-11-14 05:42:21 -0300

    Conexão de componentes na MainActivity

--- app/src/main/java/br/com/analuisa936_juliocesar94543/MainActivity.kt
+++ app/src/main/java/br/com/analuisa936_juliocesar94543/MainActivity.kt
@@ -1,22 +1,61 @@
 package br.com.analuisa936_juliocesar94543
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.analuisa936_juliocesar94543.ui.theme.AnaLuisa936_JulioCesar94543Theme
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import br.com.analuisa936_juliocesar94543.viewModel.DicasAdapter
+import br.com.analuisa936_juliocesar94543.viewModel.DicasViewModel
+import br.com.analuisa936_juliocesar94543.viewModel.DicasViewModelFactory
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: DicasViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Eco Dicas"
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val dicasAdapter = DicasAdapter { dica -> viewModel.removeDica(dica) }
+        recyclerView.adapter = dicasAdapter
+
+        val tituloInput = findViewById<EditText>(R.id.tituloInput)
+        val descInput = findViewById<EditText>(R.id.descInput)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val titulo = tituloInput.text.toString()
+            val descricao = descInput.text.toString()
+
+            if (titulo.isEmpty()) {
+                tituloInput.error = "Preencha o título"
+                return@setOnClickListener
+            }
+
+            if (descricao.isEmpty()) {
+                descInput.error = "Preencha a descrição"
+                return@setOnClickListener
+            }
+
+            viewModel.addDica(titulo, descricao)
+
+            tituloInput.text.clear()
+            descInput.text.clear()
+        }
+
+        val viewModelFactory = DicasViewModelFactory(application)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(DicasViewModel::class.java)
+
+        viewModel.dicasLiveData.observe(this) { dicas ->
+            dicasAdapter.updateDicas(dicas)
+        }
     }
 }
