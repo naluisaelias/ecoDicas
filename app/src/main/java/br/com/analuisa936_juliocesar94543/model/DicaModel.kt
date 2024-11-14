2a8d5b6c4f03fd389b6e1cb89ba5756e1d5c0823
Ana Luísa <naluisaelias@gmail.com>
2024-11-14 05:39:37 -0300

    Class Model

--- app/src/main/java/br/com/analuisa936_juliocesar94543/model/DicaModel.kt
+++ app/src/main/java/br/com/analuisa936_juliocesar94543/model/DicaModel.kt
@@ -1,4 +1,13 @@
 package br.com.analuisa936_juliocesar94543.model
 
-class DicaModel {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class DicaModel (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val titulo: String,
+    val descricao: String
+)
